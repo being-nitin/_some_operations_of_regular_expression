672cd4931dc1ea64e6f9c0da9c1962fede2a5970
Nitin <nsinghboss02@gmail.com>
2021-12-14 18:25:54 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,15 @@
+package com.codechef;
+import java.util.regex.*;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Regular Expression;
+	 */
+        // it will return true or false according to the matching of string:
+        System.out.println(Pattern.matches("nitin","nitin"));
+
+
+
+    }
+}
