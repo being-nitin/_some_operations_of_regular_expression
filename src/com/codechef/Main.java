e709d10c79f79d844fa45013aba6cba34994b7e1
Nitin <nsinghboss02@gmail.com>
2021-12-14 18:30:00 +0530

    new concept 1 added

--- src/com/codechef/Main.java
+++ src/com/codechef/Main.java
@@ -7,9 +7,14 @@ public class Main {
 	Regular Expression;
 	 */
         // it will return true or false according to the matching of string:
+
         System.out.println(Pattern.matches("nitin","nitin"));
 
+        /* . means that there can be any number of characters and second place is filled with
+        s which means the length will be two and the second character should be s,
+        will return true if the format of another string will same and false if not:
+         */
 
-
+        System.out.println(Pattern.matches(".s","as"));
     }
 }
